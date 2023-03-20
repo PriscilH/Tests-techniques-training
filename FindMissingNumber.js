926ee6a6eb821ffc4cc6746e2d6b2d79268ce811
Priscilla <comptepro@mbpdepriscilla.home>
2023-03-20 17:13:49 +0100

    add 2 exemples for test 2

--- FindMissingNumber.js
+++ FindMissingNumber.js
@@ -44,4 +44,70 @@ function findMissingNumber(arr) {
 //   Enfin, nous appelons la fonction findMissingNumber() trois fois avec des tableaux différents 
 //   pour vérifier les résultats.
   
+// Exemple 2 :
+
+// function findMissingNumber(arr) {
+
+  // Trie le tableau pour s'assurer que les nombres sont dans l'ordre croissant
+//   arr.sort((a, b) => a - b);
   
+  // Initialise la variable pour stocker le nombre manquant
+//   let missingNumber;
+  
+  // Parcourt le tableau et vérifie s'il y a un écart entre les nombres
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     if (arr[i + 1] - arr[i] > 1) {
+//       missingNumber = arr[i] + 1;
+//       break;
+//     }
+//   }
+  
+  // Si aucun nombre n'est manquant, retourne le nombre suivant la séquence
+//   if (!missingNumber) {
+//     missingNumber = arr[arr.length - 1] + 1;
+//   }
+  
+//   return missingNumber;
+// }
+
+// console.log(findMissingNumber([1, 3, 5, 4])); // 2
+// console.log(findMissingNumber([6, 3, 4, 5, 2])); // 1
+// console.log(findMissingNumber([5, 3, 2, 1, 4])); // 6
+
+// Cette solution est similaire à la précédente, mais utilise une variable pour stocker le nombre 
+// manquant plutôt que de retourner immédiatement la valeur. 
+// La fonction parcourt le tableau et vérifie s'il y a un écart entre les nombres. 
+// Si un écart est trouvé, la fonction stocke le nombre manquant dans la variable et sort de la boucle.
+// Sinon, la fonction utilise la même logique que la solution précédente pour retourner 
+// le nombre suivant la séquence.
+
+// Exemple 3 :
+
+// function findMissingNumber(arr) {
+
+  // Calculer la somme totale de la séquence en utilisant la formule de la somme d'une suite 
+//   arithmétique
+//   const totalSum = ((arr.length + 1) * (arr[0] + arr[arr.length - 1])) / 2;
+  
+  // Calculer la somme réelle des nombres dans le tableau
+//   const actualSum = arr.reduce((sum, num) => sum + num, 0);
+  
+  // Calculer la différence entre la somme totale et la somme réelle pour trouver le nombre manquant
+//   const missingNumber = totalSum - actualSum;
+  
+  // Si aucun nombre n'est manquant, retourne le nombre suivant la séquence
+//   if (missingNumber === 0) {
+//     return arr[arr.length - 1] + 1;
+//   }
+  
+//   return missingNumber;
+// }
+
+// console.log(findMissingNumber([1, 3, 5, 4])); // 2
+// console.log(findMissingNumber([6, 3, 4, 5, 2])); // 1
+// console.log(findMissingNumber([5, 3, 2, 1, 4])); // 6
+// Cette solution utilise une approche mathématique pour trouver le nombre manquant. 
+// La fonction utilise la formule de la somme d'une suite arithmétique pour calculer la somme totale 
+// de la séquence. Ensuite, elle utilise la méthode reduce() pour calculer la somme réelle des nombres 
+// dans le tableau. La différence entre la somme totale et la somme réelle donne le nombre manquant. 
+// Si aucun nombre n'est manquant, la fonction retourne le nombre suivant la séquence.
